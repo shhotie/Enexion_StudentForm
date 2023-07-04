be96ba6bd7d58d5e235b4da7f9c4212da1f39286
shhotie <swatiwanjari2@gmail.com>
2023-07-04 11:32:34 +0530

    Enexion Company Assignment for Fresher

---
+++ StudentRegistration/StudentRegistration/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentRegistration.Models;
+
+namespace StudentRegistration.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            // Retrieve all registered students from the database
+            List<Student> studentList = Student.GetStudents();
+            return View(studentList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                Student.AddStudent(student);
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        public ActionResult Clear()
+        {
+            ModelState.Clear();
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ StudentRegistration/StudentRegistration/Models/Student.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Xml.Linq;
+
+namespace StudentRegistration.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Language is required")]
+        public string Language { get; set; }
+
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Student;Integrated Security=True";
+            try
+            {
+                con.Open();
+                SqlCommand cmdList = new SqlCommand();
+                cmdList.Connection = con;
+                cmdList.CommandType = CommandType.Text;
+                cmdList.CommandText = "select * from Students";
+                SqlDataReader dr = cmdList.ExecuteReader();
+                while (dr.Read())
+                {
+                    studentList.Add(new Student
+                    {
+                        Id = dr.GetInt32("Id"),
+                        Name = dr.GetString("Name"),
+                        DOB = (DateTime)dr["DOB"],
+                        Email = dr["Email"].ToString(),
+                        Gender = dr["Gender"].ToString(),
+                        Language = dr["Language"].ToString()
+                    });
+                }
+                dr.Close();
+                return studentList;
+            }
+            catch
+            {
+                throw;
+            }
+            finally { con.Close(); }
+        }
+
+        public static void AddStudent(Student student)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Student;Integrated Security=True";
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Insert into Students values (@Name, @DOB, @Email, @Gender, @Language)";
+                
+                cmd.Parameters.AddWithValue("@Name", student.Name);
+                cmd.Parameters.AddWithValue("@DOB", student.DOB);
+                cmd.Parameters.AddWithValue("@Email", student.Email);
+                cmd.Parameters.AddWithValue("@Gender", student.Gender);
+                cmd.Parameters.AddWithValue("@Language", student.Language);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch { throw; }
+            finally { con.Close(); }
+
+        }
+    }
+}
